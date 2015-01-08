7e21e963f8e854f5e389fbfa8fa01bece9b01e37
Kartik Jain <kartikj2012@gmail.com>
2015-01-08 22:08:00 +0530

    all code shifted to app.js

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var express = require('express'),
-    stickies = require('./routes/stickies');
- 
+var express = require('express');
+var mongojs = require('mongojs'); 
 var app = express();
- 
-
+var collections = ['ngos','stickies'];
+var databaseURI = process.env.MONGO_URL || "localhost:27017/ngoserver";
 var port = process.env.PORT || 3000;
+var db = mongojs(databaseURI,collections);
 
 app.configure(function () {
     app.use(express.logger('dev'));     
     app.use(express.bodyParser());
 });
 
+app.listen(port);
 /* enabling CORS see : http://www.html5rocks.com/en/tutorials/cors/ */
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,11 +19,37 @@ app.all('*', function(req, res, next) {
   next();
  });
 
-
-app.get('/', stickies.findAll);
-app.get('/pinboard', stickies.findAll);
-app.get('/pinboard/:id', stickies.detaild);
-app.get('/test', stickies.test);
- 
-app.listen(port);
 console.log('Listening on port 3000...');
+app.get('/', function(req, res) {
+    db.stickies.find(function(err, docs) {
+
+       // docs.toArray(function(err, items) {
+            res.send(docs);
+            console.log('sending all ....')
+        //});
+    });
+
+});
+app.get('/pinboard', function(req, res) {
+    db.stickies.find(function(err, docs) {
+
+       // docs.toArray(function(err, items) {
+            res.send(docs);
+            console.log('sending all ....')
+        //});
+    });
+
+});
+
+app.get('/pinboard/:id', function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving stickies: ' + id);
+    db.stickies.findOne({'_id':mongojs.ObjectId(id)}, function(err, doc) {
+        
+            if(err)
+                res.send(error);
+
+            res.send(doc);
+        });
+   
+});
